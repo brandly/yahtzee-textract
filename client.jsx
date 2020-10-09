e21f171b2afc8a9b3b9a3d84d7fadc987d40029f
brandly <brandly13@gmail.com>
2020-10-09 14:12:44 -0400

    show top computed values

--- client.jsx
+++ client.jsx
@@ -2,6 +2,7 @@ const React = require('react')
 const { useState, useEffect } = React
 const { render } = require('react-dom')
 const { columnsToRows } = require('./read')
+const { sum } = require('lodash')
 
 const baseUrl =
   process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:1235/api'
@@ -21,6 +22,8 @@ const App = () => {
   if (games === null) return 'Loading...'
   const rows = columnsToRows(games)
 
+  const gameForX = (x) => games.find((g) => g[0].coords.x === x)
+
   return (
     <table>
       <thead>
@@ -33,14 +36,36 @@ const App = () => {
       <tbody>
         {rows.slice(1).map((row) => (
           <tr key={row[0].coords.y}>
-            {row.map((cell) => {
-              const missing = cell.parsed === undefined
+            {row.map(({ coords, parsed, text, confidence, field }) => {
+              const missing = parsed === undefined
+              if (field === 'topSubtotal') {
+                return (
+                  <td key={coords.x} style={{ fontWeight: 'bold' }}>
+                    {getTopSubtotal(gameForX(coords.x))}
+                  </td>
+                )
+              }
+              if (field === '63Bonus') {
+                return (
+                  <td key={coords.x} style={{ fontWeight: 'bold' }}>
+                    {getTopSubtotal(gameForX(coords.x)) >= 63 ? 35 : '-'}
+                  </td>
+                )
+              }
+              if (field === 'topTotal') {
+                const topSubtotal = getTopSubtotal(gameForX(coords.x))
+                return (
+                  <td key={coords.x} style={{ fontWeight: 'bold' }}>
+                    {topSubtotal >= 63 ? 35 + topSubtotal : topSubtotal}
+                  </td>
+                )
+              }
               return (
                 <td
-                  key={cell.coords.x}
-                  style={{ color: missing ? 'red' : 'black' }}
+                  key={coords.x}
+                  style={{ color: confidence < 70 ? 'red' : 'black' }}
                 >
-                  {missing ? <input type="text" /> : cell.parsed}
+                  {missing ? <input type="text" /> : parsed}
                 </td>
               )
             })}
@@ -50,5 +75,14 @@ const App = () => {
     </table>
   )
 }
+
+const getTopSubtotal = (game) =>
+  sum(
+    ['aces', 'twos', 'threes', 'fours', 'fives', 'sixes'].map(
+      (f) => getFieldByName(game, f).parsed
+    )
+  )
+
+const getFieldByName = (game, field) => game.find((c) => c.field === field)
 
 render(<App />, document.getElementById('main'))
